0391d5be59e854a8cc9d24015b5d82ec250b749e
IJEN00 <ferni.b@seznam.cz>
2025-12-13 00:16:39 +0100

    Updated dashboard

--- InventoryApp/Controllers/HomeController.cs
+++ InventoryApp/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using InventoryApp.Models;
 using InventoryApp.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace InventoryApp.Controllers
 {
@@ -9,23 +11,66 @@ namespace InventoryApp.Controllers
         public int TotalQuantity { get; set; }
         public int LowStockCount { get; set; }
         public List<InventoryApp.Models.Component> LowStock { get; set; } = new();
+
+        public List<string> ConsumptionLabels { get; set; } = new();
+        public List<int> ConsumptionValues { get; set; } = new();
     }
 
     public class HomeController : Controller
     {
         private readonly IComponentService _components;
-        public HomeController(IComponentService components) => _components = components;
+        private readonly AppDbContext _context;
+
+        public HomeController(IComponentService components, AppDbContext context)
+        {
+            _components = components;
+            _context = context;
+        }
 
         public async Task<IActionResult> Index()
         {
             var low = await _components.GetLowStockAsync();
+
+            var since = DateTime.UtcNow.AddDays(-30);
+
+            var consumption = await _context.InventoryTransactions
+                .Where(t => t.CreatedAt >= since && t.DeltaQuantity < 0)
+                .GroupBy(t => t.ComponentId)
+                .Select(g => new
+                {
+                    ComponentId = g.Key,
+                    Used = -g.Sum(x => x.DeltaQuantity)
+                })
+                .OrderByDescending(x => x.Used)
+                .Take(7)
+                .ToListAsync();
+
+            var componentIds = consumption.Select(x => x.ComponentId).ToList();
+
+            var components = await _context.Components
+                .Where(c => componentIds.Contains(c.Id))
+                .Select(c => new { c.Id, c.Name })
+                .ToListAsync();
+
+            var labels = new List<string>();
+            var values = new List<int>();
+
+            foreach (var c in consumption)
+            {
+                var name = components.FirstOrDefault(x => x.Id == c.ComponentId)?.Name ?? "Unknown";
+                labels.Add(name);
+                values.Add(c.Used);
+            }
 
             var model = new HomeViewModel
             {
                 TotalComponents = await _components.GetTotalComponentsAsync(),
                 TotalQuantity = await _components.GetTotalQuantityAsync(),
                 LowStockCount = low.Count,
-                LowStock = low.Take(10).ToList()
+                LowStock = low.Take(10).ToList(),
+
+                ConsumptionLabels = labels,
+                ConsumptionValues = values
             };
 
             return View(model);
